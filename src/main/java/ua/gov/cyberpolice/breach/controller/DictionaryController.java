174f10e627a1dba8abbbc556198ce3a95fe21094
Alexey Osadchiy <Alexey.Osadchiy@gmail.com>
2021-01-23 00:52:07 +0200

    Edit dictionary

--- src/main/java/ua/gov/cyberpolice/breach/controller/DictionaryController.java
+++ src/main/java/ua/gov/cyberpolice/breach/controller/DictionaryController.java
@@ -3,11 +3,12 @@ package ua.gov.cyberpolice.breach.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ua.gov.cyberpolice.breach.entity.*;
 import ua.gov.cyberpolice.breach.repository.*;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 import java.util.Map;
 import java.util.UUID;
 
@@ -15,7 +16,7 @@ import java.util.UUID;
 @RequestMapping("dictionary")
 public class DictionaryController {
 
-    private static final String VIEWS_BREACH_DICTIONARY_FORM = "breach/createOrUpdateDictionaryForm";
+    private static final String CREATE_OR_UPDATE_DICTIONARY_FORM = "breach/createOrUpdateDictionaryForm";
 
     private final DictionaryRepository dictionaryRepository;
     private final BreachTypeRepository breachTypeRepository;
@@ -41,48 +42,69 @@ public class DictionaryController {
     }
 
     @GetMapping
-    public String processListDictionaryForm(Map<String, Object> model) {
-        Iterable<Dictionary> dictionaries = dictionaryRepository.findAll();
+    public String processViewDictionaryForm(Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
 
-        model.put("dictionaries", dictionaries);
-        return VIEWS_BREACH_DICTIONARY_FORM;
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
     }
 
-    @GetMapping("{dictionaryType}")
-    public String processEditDictionaryForm(
-            @PathVariable("dictionaryType") String dictionaryType,
-            Map<String, Object> model) {
-        Iterable<Dictionary> dictionaries = dictionaryRepository.findAll();
-        Iterable<BreachType> breachTypes = null;
-        Iterable<BreachMethod> breachMethods = null;
-        Iterable<ParticipantType> participantTypes = null;
-        Iterable<Prison> prisons = null;
-        Iterable<PaymentProvider> paymentProviders = null;
+    @GetMapping("breach-type")
+    public String processViewBreachTypeForm(Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
+        model.put("breachTypes", breachTypeRepository.findAll());
+        model.put("breachType", new BreachType());
 
-        switch (dictionaryType) {
-            case "breach-type":
-                breachTypes = breachTypeRepository.findAll();
-                break;
-            case "breach-method":
-                breachMethods = breachMethodRepository.findAll();
-                break;
-            case "participant-type":
-                participantTypes = participantTypeRepository.findAll();
-                break;
-            case "prison":
-                prisons = prisonRepository.findAll();
-                break;
-            case "payment-provider":
-                paymentProviders = paymentProviderRepository.findAll();
-                break;
-        }
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
+    }
 
-        model.put("dictionaries", dictionaries);
-        model.put("breachTypes", breachTypes);
-        model.put("breachMethods", breachMethods);
-        model.put("participantTypes", participantTypes);
-        model.put("prisons", prisons);
-        model.put("paymentProviders", paymentProviders);
-        return VIEWS_BREACH_DICTIONARY_FORM;
+    @GetMapping("breach-type/{breachTypeId}")
+    public String processEditBreachTypeForm(@PathVariable("breachTypeId") Integer breachTypeId, Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
+        model.put("breachTypes", breachTypeRepository.findAll());
+        model.put("breachType", breachTypeRepository.findById(breachTypeId).orElse(new BreachType()));
+
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
+    }
+
+    @GetMapping("breach-method")
+    public String processViewBreachMethodForm(Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
+        model.put("breachMethods", breachMethodRepository.findAll());
+        model.put("breachMethod", new BreachMethod());
+
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
+    }
+
+    @GetMapping("participant-type")
+    public String processViewParticipantTypeForm(Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
+        model.put("participantTypes", participantTypeRepository.findAll());
+        model.put("participantType", new ParticipantType());
+
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
+    }
+
+    @GetMapping("prison")
+    public String processViewPrisonForm(Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
+        model.put("prisons", prisonRepository.findAll());
+        model.put("prison", new Prison());
+
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
+    }
+
+    @GetMapping("payment-provider")
+    public String processViewPaymentProviderForm(Map<String, Object> model) {
+        model.put("dictionaries", dictionaryRepository.findAll());
+        model.put("paymentProviders", paymentProviderRepository.findAll());
+        model.put("paymentProvider", new PaymentProvider());
+
+        return CREATE_OR_UPDATE_DICTIONARY_FORM;
+    }
+
+    @PostMapping("breach-type")
+    public String processForm(@Valid BreachType breachType) {
+        this.breachTypeRepository.save(breachType);
+        return "redirect:/dictionary/breach-type";
     }
 }
